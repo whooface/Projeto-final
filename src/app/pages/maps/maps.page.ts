a54150d5f3846dd18c18a0834a57b011db04a706
higorosantos <45833012+higorosantos@users.noreply.github.com>
2019-12-02 21:44:17 -0300

    ok

---
+++ src/app/pages/maps/maps.page.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.page.html',
+  styleUrls: ['./maps.page.scss'],
+})
+export class MapsPage implements OnInit {
+  constructor(
+    
+  ) { }
+
+  ngOnInit() {
+    this.loadMap()
+  }
+
+  map: GoogleMap
+  loadMap() {
+
+    // This code is necessary for browser
+
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Senac Campo Grande',
+      icon: 'orange',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+
+}
