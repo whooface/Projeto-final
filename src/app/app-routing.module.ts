5c43408ddf767ae99d8daeeb9834b4ab439c8d26
higorosantos <45833012+higorosantos@users.noreply.github.com>
2020-01-15 20:39:32 -0300

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,18 +20,18 @@ const routes: Routes = [
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
   },
   { path: 'adduser',loadChildren: './pages/adduser/adduser.module#AdduserPageModule' },
-  { path: 'adddog',loadChildren: './pages/adddog/adddog.module#AdddogPageModule', },
+  { path: 'adddog',loadChildren: './pages/adddog/adddog.module#AdddogPageModule', canActivate:[GuardService]},
   { path: 'perfildog/:id', loadChildren: './pages/perfildog/perfildog.module#PerfildogPageModule',  },
-  { path: 'perfiluser', loadChildren: './pages/perfiluser/perfiluser.module#PerfiluserPageModule' },
+  { path: 'perfiluser', loadChildren: './pages/perfiluser/perfiluser.module#PerfiluserPageModule',canActivate:[GuardService] },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'googlemaps', loadChildren: './pages/googlemaps/googlemaps.module#GooglemapsPageModule', canActivate:[GuardService] },
-  { path: 'updateuser', loadChildren: './pages/updateuser/updateuser.module#UpdateuserPageModule' },
-  { path: 'trocar-senha', loadChildren: './pages/trocar-senha/trocar-senha.module#TrocarSenhaPageModule' },
-  { path: 'contatos', loadChildren: './pages/contatos/contatos.module#ContatosPageModule' },
-  { path: 'chat', loadChildren: './pages/chat/chat.module#ChatPageModule' },
-  { path: 'notificacao', loadChildren: './pages/notificacao/notificacao.module#NotificacaoPageModule' },
-  { path: 'galeria-dog', loadChildren: './pages/galeria-dog/galeria-dog.module#GaleriaDogPageModule' },
-  { path: 'favoritos', loadChildren: './pages/favoritos/favoritos.module#FavoritosPageModule' }
+  { path: 'updateuser', loadChildren: './pages/updateuser/updateuser.module#UpdateuserPageModule' ,canActivate:[GuardService]},
+  { path: 'trocar-senha', loadChildren: './pages/trocar-senha/trocar-senha.module#TrocarSenhaPageModule' ,canActivate:[GuardService]},
+  { path: 'contatos', loadChildren: './pages/contatos/contatos.module#ContatosPageModule' ,canActivate:[GuardService]},
+  { path: 'chat', loadChildren: './pages/chat/chat.module#ChatPageModule' ,canActivate:[GuardService]},
+  { path: 'notificacao', loadChildren: './pages/notificacao/notificacao.module#NotificacaoPageModule',canActivate:[GuardService]},
+  { path: 'galeria-dog', loadChildren: './pages/galeria-dog/galeria-dog.module#GaleriaDogPageModule' ,canActivate:[GuardService]},
+  { path: 'favoritos', loadChildren: './pages/favoritos/favoritos.module#FavoritosPageModule' ,canActivate:[GuardService]}
 
 
 
